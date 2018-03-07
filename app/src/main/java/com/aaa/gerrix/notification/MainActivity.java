a3cefcd910ed0bc774b2e9f710882908ac8b4380
Gerrix <jahja.trifunovic@gmail.com>
2018-03-07 11:26:24 +0100

    Added notification update

--- app/src/main/java/com/aaa/gerrix/notification/MainActivity.java
+++ app/src/main/java/com/aaa/gerrix/notification/MainActivity.java
@@ -1,6 +1,5 @@
 package com.aaa.gerrix.notification;
 
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -9,6 +8,7 @@ import android.os.Bundle;
 import android.support.v4.app.NotificationCompat.Action;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.NotificationCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -20,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
     private static final String NO_ACTION = "com.tinbytes.simplenotificationapp.NO_ACTION";
 
     private NotificationManager notificationManager;
+//    private Notification notification;
+    private boolean  firstTime = true;
+    private NotificationCompat.Builder notification = new NotificationCompat.Builder(this);
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
         Intent noIntent = getNotificationIntent();
         noIntent.setAction(NO_ACTION);
 
-        Notification notification = new NotificationCompat.Builder(this)
+        notification = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
                 .setContentIntent(PendingIntent.getActivity(this, 0, getNotificationIntent(), PendingIntent.FLAG_UPDATE_CURRENT))
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setTicker("Action Buttons Notification Received")
@@ -76,10 +80,17 @@ public class MainActivity extends AppCompatActivity {
                 .addAction(new Action(
                         R.mipmap.ic_thumb_down_black_36dp,
                         getString(R.string.no),
-                        PendingIntent.getActivity(this, 0, noIntent, PendingIntent.FLAG_UPDATE_CURRENT)))
-                .build();
+                        PendingIntent.getActivity(this, 0, noIntent, PendingIntent.FLAG_UPDATE_CURRENT)));
 
-        notificationManager.notify(NOTIFY_ID, notification);
+        update(null);
+    }
+
+    private void update(String text) {
+        if (!firstTime){
+            notification.setContentTitle(text + " clicked!");
+        }
+        notificationManager.notify(NOTIFY_ID, notification.build());
+        firstTime = false;
     }
 
     @Override
@@ -92,12 +103,15 @@ public class MainActivity extends AppCompatActivity {
         if (intent.getAction() != null) {
             switch (intent.getAction()) {
                 case YES_ACTION:
+                    update("Yes");
                     Toast.makeText(this, "Yes :)", Toast.LENGTH_SHORT).show();
                     break;
                 case MAYBE_ACTION:
+                    update("Maybe");
                     Toast.makeText(this, "Maybe :|", Toast.LENGTH_SHORT).show();
                     break;
                 case NO_ACTION:
+                    update("No");
                     Toast.makeText(this, "No :(", Toast.LENGTH_SHORT).show();
                     break;
             }
